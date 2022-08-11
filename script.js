2c90773d049390864498950caa4b8676a869da9c
unknown <anonymous.alias.010@gmail.com>
2022-08-10 23:44:23 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,3 @@
+let l=document.getElementById("ot");
+let ar=["take a walk outside","plant a tree","make a garden","clean trash in your community","camping outside","not wasting water","reuse reduce and recycle","walking all day", "bike more and drive less","volunteer with environmental organizations","save resources","decrease meat & dairy consumption"];
+l.innerHTML=ar[Math.floor(Math.random()*ar.length)];
